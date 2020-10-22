ab3db5f7cae32180f118bd6c87de2ff33661c362
AB <fbd.1393@gmail.com>
2020-10-22 14:06:38 +0330

    chenge settings, create models and migration file to make tables on database

--- travello/models.py
+++ travello/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+# for making a database tables we have to change our class
+# we have to add
 
-class Destination:
-    id: int
-    name: str
-    desc: str
-    price: str
-    img: str
-    offer: bool
+
+class Destination (models.Model):
+
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.IntegerField()
+    img = models.ImageField(upload_to='travello_img')
+    offer = models.BooleanField(default=False)
+
+
+1
